f4e361617902001ec1926cac364ef780c7271417
Jordan <jordanhaddadi@gmail.com>
2019-01-28 12:13:21 -0500

    added local strategy

---
+++ passport/local.js
@@ -0,0 +1,33 @@
+const { Strategy: LocalStrategy } = require('passport-local');
+const User = require('./userschema');
+
+const LocalStrategy = new LocalStrategy((username, password, done)
+    => {
+        let user; 
+        User.findOne({ username })
+            .then(results => {
+                user = results;
+                if (!results){
+                    return Promise.reject({
+                        reason: 'LoginError',
+                        message: 'Incorrect username',
+                        location: 'username'
+                    })
+            }
+        const isValid = user.validatePassword(password);
+            if (!isValid){
+                return Promise.reject({
+                    reason: 'LoginError',
+                    message: 'Incorrect password',
+                    location: 'password'
+                });
+            }
+            return done(null,user);
+        })
+        .catch(err => {
+            if (err.reason === 'LoginErro'){
+                return done(null, false);
+            }
+            return done(err);
+        });
+    });
