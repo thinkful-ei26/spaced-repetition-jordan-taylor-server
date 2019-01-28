8742da86d981b9c8614a771baad3a5ad9515c7d9
taylormerrill <taylormerrill92@gmail.com>
2019-01-28 11:33:16 -0700

    user/auth routes

--- passport/jwt.js
+++ passport/jwt.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const passportJWT = require ('passport-jwt');
-
 const{ Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const { JWT_SECRET } = require('../config'); 
 
--- passport/local.js
+++ passport/local.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-
-const { Strategy: LocalStrategy } = require('passport-local');
-
-const User = require('../models/user');
-
-// ===== Define and create basicStrategy =====
-const localStrategy = new LocalStrategy((username, password, done) => {
-  let user;
-  User.findOne({ username })
-    .then(results => {
-      user = results;
-      if (!user) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username',
-          location: 'username'
-        });
-      }
-      return user.validatePassword(password);
-    })
-    .then(isValid => {
-      if (!isValid) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect password',
-          location: 'password'
-        });
-      }
-      return done(null, user);
-    })
-    .catch(err => {
-      if (err.reason === 'LoginError') {
-        return done(null, false);
-      }
-      return done(err);
-    });
-});
-
-module.exports = localStrategy;
-=======
 const { Strategy: LocalStrategy } = require('passport-local');
 const User = require('./userschema');
 
@@ -67,10 +25,9 @@ const LocalStrategy = new LocalStrategy((username, password, done)
             return done(null,user);
         })
         .catch(err => {
-            if (err.reason === 'LoginErro'){
+            if (err.reason === 'LoginError'){
                 return done(null, false);
             }
             return done(err);
         });
     });
->>>>>>> f4e361617902001ec1926cac364ef780c7271417
