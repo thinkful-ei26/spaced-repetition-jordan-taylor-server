11b80c95c284b5f7e0da38463c7607931a388cd9
taylormerrill <taylormerrill92@gmail.com>
2019-01-28 10:23:04 -0700

    pulling

--- passport/jwt.js
+++ passport/jwt.js
@@ -3,4 +3,16 @@
 const passportJWT = require ('passport-jwt');
 
 const{ Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const { JWT_SECRET } = require('../config')
+const { JWT_SECRET } = require('../config'); 
+
+const options = {
+    secretOrKey: JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    algorithms: ['HS256']
+  };
+  
+  const jwtStrategy = new JwtStrategy(options, (payload, done) => {
+    done(null, payload.user);
+  });
+  
+  module.exports = jwtStrategy;
--- userschema.js
+++ userschema.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    fullname: String,
+    firstName: String,
+    lastName:String,
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 userSchema.set('toJSON', {
