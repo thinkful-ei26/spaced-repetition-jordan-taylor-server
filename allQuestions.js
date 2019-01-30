12cf135b6a70dcb23ed9aa08df99cc88774810a0
Jordan <jordanhaddadi@gmail.com>
2019-01-30 18:20:22 -0500

    updated questions and answers

--- allQuestions.js
+++ allQuestions.js
@@ -1,135 +1,135 @@
 const questionsArray = [
     {
-    "question":"What does ES6 refer to?",
-    "answer":"Most recent version of the ECMAScript Language Specificaiton", 
+    "question":"gast",
+    "answer":"stomach", 
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What is JSX?",
-    "answer":"A syntax extension to JavaScript that uses camel case and had the full power of JavaScript.",    
+    "question":"cardi",
+    "answer":"heard",    
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"In React, what are Elements?", 
-    "answer":"The building blocks of React applications. React elements describe what you want to see on the screen, and are immutable.",   
+    "question":"megal", 
+    "answer":"enlarged",   
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number, 
     "next":Number
     },
     {
-    "question":"In React, what are Components?",
-    "answer": "Small, reusable pieces of code that return a React element to be rendered on the page.",   
+    "question":"itis",
+    "answer": "inflammation",   
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number, 
     "next":Number
     },
     {
-    "question":"In React, what are props?",
-    "answer":"Inputs to a React component. Props are data passed down from a parent component to a child component.",    
+    "question":"dermat",
+    "answer":"skin",    
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number,
     },
     {
-    "question":"What is the functionality of props.children?",
-    "answer":"Available on every component, props.children contains the content between the opening and closing tags of a component.",    
+    "question":"cerebr",
+    "answer":"brain",    
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"In React, what is State?",
-    "answer":"Component data that changes over time",   
+    "question":"path",
+    "answer":"disease",   
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What are Lifecycle Methods?",
-    "answer": "Custom functionality that gets executed during different phases of a component.",
+    "question":"enter",
+    "answer": "intestines",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What are Controlled Components?",
-    "answer": "Values that rely on React to perform",
+    "question":"aden",
+    "answer": "gland",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     }, 
     {
-    "question":"What are Uncontrolled Components?",
-    "answer": "Values that do not rely on React to perform ",
+    "question":"angi",
+    "answer": "vessel",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What are Keys?",
-    "answer": "A special string attribute needed to create arrays of elements.",
+    "question":"oma",
+    "answer": "tumor",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What are Refs?",
-    "answer": "A special attribute that can be attached to any component and used to have direct access to the DOM element or the component itself. Refs can be objects, callback functions, or strings.",
+    "question":"nephr",
+    "answer": "kidney",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     }, 
     {
-    "question":"What is React?",
-    "answer": "React is a JavaScript library",
+    "question":"hepat",
+    "answer": "liver",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     }, 
     {
-    "question":"Is React a framework?",
-    "answer":"No, React is a library",
+    "question":"athr",
+    "answer":"joint",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What is React used for?",
-    "answer":"React is used to build user interfaces (UI) on the front end",
+    "question":"blephar",
+    "answer":"eyelid",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"When you import React at the top of a new file, what are you importing exactly?",
-    "answer": "You are importing the React top level API",
+    "question":"ologist",
+    "answer": "specialist",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number,
     "next":Number
     },
     {
-    "question":"What is the corelation between React and Babel?",
-    "answer":"Babel is a JavaScript compiler that lets us use ES6+ in old browsers",
+    "question":"malacia",
+    "answer":"soft",
     "correct":false, 
     "guessed":false,
     "memoryStrength":Number, 
