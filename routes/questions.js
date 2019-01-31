53fb51fdf2d4542b5cbc62176275d38dea945a3b
Jordan <jordanhaddadi@gmail.com>
2019-01-30 20:50:18 -0500

    getting the next value back from put endpoint yay!!

--- routes/questions.js
+++ routes/questions.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
       .then(result => {
        return res.json({
                current:result[0].currentQuestion.head.value,
-               next:result[0].currentQuestion.head.next.value
+              //  next:result[0].currentQuestion.head.next.value
        });    
       })
       .catch(err => {
@@ -21,16 +21,14 @@ router.get('/', (req, res, next) => {
       });
 });
 
-router.put('/', (req, res, next) => {
+router.put('/current', (req, res, next) => {
   //get the answer that the user sent
   let answer = req.body.answer
   let userId = req.user.id;
-  console.log(answer)
   
   //compare that with the correct answer from the current question
   User.find({_id:userId})
       .then(result => {
-
         if(result[0].currentQuestion.head.value.answer === answer){
           result[0].currentQuestion.head.value.guessAttempts++
           result[0].currentQuestion.head.value.memoryStrength++
@@ -39,20 +37,27 @@ router.put('/', (req, res, next) => {
           let temp = result[0].currentQuestion.head.next.value
 
           console.log('Correct!')
-          console.log(result[0].currentQuestion.head.value.guessAttempts++)
           console.log(currentHead)
 
           currentHead = temp
-
-          return (
-            console.log(currentHead), result[0]
-          )
+          console.log(currentHead), result[0]
         }  
         else {
           result[0].currentQuestion.head.value.memoryStrength--
+          result[0].currentQuestion.head.value.guessAttempts++
+
+          let currentHead = result[0].currentQuestion.head.value
+          let temp = result[0].currentQuestion.head.next.value
+
           console.log('Incorrect')
+          console.log(currentHead)
+
+          currentHead = temp
+          console.log(currentHead), result[0]
         }
+
         return res.json({
+          current:result[0].currentQuestion.head.next.value
         }) 
       })
       .catch(err => {
