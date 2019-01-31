90d41ad91097bbc9464ee78b6a2b2c769fac86a2
taylormerrill <taylormerrill92@gmail.com>
2019-01-31 14:49:53 -0700

    committing for merge

--- models/userschema.js
+++ models/userschema.js
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema({
   lastName: { type: String, default: '' },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }, 
-  currentQuestion:{ type:Object },
+  currentQuestion:{ type:Object, default: questionsArray  },
 });
 
 // Transform output during `res.json(data)`, `console.log(data)` etc.
