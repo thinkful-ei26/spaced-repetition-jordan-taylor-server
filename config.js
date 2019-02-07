c3741bdb45091d80e3eb24a602ea95f64cbcb990
taylormerrill <taylormerrill92@gmail.com>
2019-02-07 10:44:16 -0700

    changed db

--- config.js
+++ config.js
@@ -6,10 +6,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://jordantaylor:spaced1@ds113693.mlab.com:13693/spaced-repetition',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
+        process.env.DATABASE_URL || 'mongodb://dev:pass123@ds125225.mlab.com:25225/spacedrepetition',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 }; 
